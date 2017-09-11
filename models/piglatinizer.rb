6a25d595ddfcd05614ecf496fdd71b8de962c1fa
Mariel F <marielbeth@gmail.com>
2017-09-11 16:13:16 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,10 +2,17 @@ class PigLatinizer
   def piglatinize(word)
     letters = word.split("")
 
-    if letters[0]
+    if letters[0].is_vowel?
       letters.join += "way"
-    elsif !letters[]
-      
+    else
+      cons = letters[0]
+      i = letters[0]
+      until !!i.is_vowel?
+        cons << i
+        i += 1
+      end
+      ordway = letters[i..-1] + cons
+      ordway.join += "ay"
   end
 
   def is_vowel?(letter)
